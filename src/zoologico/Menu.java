48e82a9a88cb1bdf46f17b620b1ca16f13ecd767
Miguel Guadalupe Ledesma Duarte <miguel.gledesma.duarte@gmail.com>
2021-04-30 21:41:56 -0600

    Se agregan datos del encargado.

--- src/zoologico/Menu.java
+++ src/zoologico/Menu.java
@@ -5,22 +5,83 @@
  */
 package zoologico;
 
-import zoologico.*;
+import java.util.Scanner;
 
-public class Menu {
+public class Menu 
+{
+    static Zoologico zo = new Zoologico(); //Objeto Zoologico creado.
+    //ingreso por consola.
+    public static Scanner inp = new Scanner(System.in);
+    public static int opcion;
+    public static boolean exist = false;
+        
+    private static void validaExistencia(){
+        if(zo.getNOMBRE()==null)
+            System.out.println("Sin zoologicos por el momento, favor de agregar.");
+        else
+            exist = true;
+    }
     
-    public void Menu(){}
-    public void SubMenu(){}
+    //Agregar Datos al objeto Zoologico
+    public static void subMenuAgregarNuevo()    {
+        //Limpiamos el input.
+        inp.next();
+        System.out.println("Ingrese los datos del Zoologico: ");
+        System.out.println("\n Nombre: ");
+        System.out.println("\n Ciudad: ");
+        System.out.println("\n Pais: ");
+        System.out.println("\n Estado actual: ");
+        System.out.println("\n Encargado: ");
+        System.out.println("Ingrese los datos del Encargado: ");
+    }
+    
+    public static void subMenuMostrar() {
+        Menu.validaExistencia();
+    }
+    
+    public static void subMenuMostrarSegunEstado()  {
+        Menu.validaExistencia();
+    }
+    
+    public static void subMenuModificar(){}
+
+    public static void Menu(int op){
+        switch(op){
+            case 1:
+                Menu.subMenuAgregarNuevo();
+                break;
+            case 2:
+                Menu.subMenuMostrar();
+                break;
+            case 3:
+                break;
+            case 4:
+                Menu.subMenuMostrarSegunEstado();
+                break;
+            case 5:
+                inp.close();
+                break;
+            default:
+                System.out.println("Ingrese una opción valida.");
+        }
+    }
     
     public static void ImprimeMenu(){
-        System.out.println(" _.: - - - - - Menu de opciones. - - - - - :._"      + 
-                "\n---------------------------------------" +
-                "\n[1].-Agregar Zoologico Nuevo."           +
-                "\n[2].-Mostrar Zoologicos."                +
-                "\n[3].-Modificar Zoologico Existente."     +
-                "\n[4].-Mostrar Zoologicos [Segun estado]"  +
-                "\n---------------------------------------" +
-                "\n[5].-Salir.");
+        while(opcion!=5) {
+                System.out.print("\n ************************************** \n");
+                System.out.println("\n _.: - - - - - Menu de opciones. - - - - - :._"      + 
+                        "\n---------------------------------------" +
+                        "\n[1].-Agregar Zoologico Nuevo."           +
+                        "\n[2].-Mostrar Zoologicos."                +
+                        "\n[3].-Modificar Zoologico Existente."     +
+                        "\n[4].-Mostrar Zoologicos [Segun estado]"  +
+                        "\n---------------------------------------" +
+                        "\n[5].-Salir.");
+                System.out.print("\n ************************************** \n");
+                System.out.print("\n Ingrese una opcion[1-5]->:");
+                opcion = inp.nextInt();
+                Menu.Menu(opcion);
+        }
     }
         
     /**
@@ -28,9 +89,7 @@ public class Menu {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Zoologico zo = new Zoologico(); //Objeto Zoologico creado.
-        System.out.print("\n ************************************** \n");
         ImprimeMenu();
-        System.out.print("\n ************************************** \n");
+        
     }
 }
